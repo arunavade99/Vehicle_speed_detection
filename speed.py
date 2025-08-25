bf87aea8323b3ac660894c4aaa00dba5efb48858
arunavade99 <arunavade99@gmail.com>
2025-08-25 20:46:29 +0530

    Implement code changes to enhance functionality and improve performance

--- speed.py
+++ speed.py
@@ -8,158 +8,125 @@ import math
 model = YOLO('yolov8n.pt')
 
 
-def RGB(event, x, y, flags, param):
+def show_mouse_position(event, x, y, flags, param):
     if event == cv2.EVENT_MOUSEMOVE:
-        colorsBGR = [x, y]
-        print(colorsBGR)
+        mouse_coords = [x, y]
+        print(mouse_coords)
 
 
 cv2.namedWindow('RGB')
-cv2.setMouseCallback('RGB', RGB)
+cv2.setMouseCallback('RGB', show_mouse_position)
 
-# cap = cv2.VideoCapture('veh2.mp4')
-cap = cv2.VideoCapture('6.mp4')
+cap = cv2.VideoCapture(r'D:\python projects\videos for project\6.mp4')
 
+frame_count = 0
+frame_times = []
 
-# my_file = open("coco.txt", "r")
-# data = my_file.read()
-# class_list = data.split("\n")
-# print(class_list)
+line_y1 = 280
+line_y2 = 330
+line_y3 = 235
+line_y4 = 290
 
-count = 0
-tcount=[]
+line_offset = 10
 
-cy1 = 280
-cy2 = 330
-cy3=235
-cy4=290
+avg_speed_down = 0
+vehicles_down = {}
+down_counter = []
 
-offset = 10
-
-a_speed_kh=0
-vh_down = {}
-counter = []
-
-a_speed_kh1=0
-vh_up = {}
-counter1 = []
+avg_speed_up = 0
+vehicles_up = {}
+up_counter = []
 
 while True:
     ret, frame = cap.read()
     if not ret:
         break
-    count += 1
-    if count % 3 != 0:
+    frame_count += 1
+    if frame_count % 3 != 0:
         continue
     frame = cv2.resize(frame, (1020, 500))
-    detections = np.empty([0, 5])
+    detections_array = np.empty([0, 5])
 
     results = model.track(frame, persist=True, conf=0.2, classes=[2], tracker="bytetrack.yaml", verbose=False)
-    list = []
-    boxes = results[0].boxes.xyxy.cpu().numpy().astype(int)
+    vehicle_list = []
+    detected_boxes = results[0].boxes.xyxy.cpu().numpy().astype(int)
     if results[0].boxes.id is None:
         pass
     else:
-        ids = results[0].boxes.id.cpu().numpy().astype(int)
+        vehicle_ids = results[0].boxes.id.cpu().numpy().astype(int)
 
-    for box, id in zip(boxes, ids):
-        x3, y3, x4, y4 = int(box[0]), int(box[1]), int(box[2]), int(box[3])
-        cx = int(box[0] + box[2]) // 2
-        cy = int(box[1] + box[3]) // 2
-        cv2.circle(frame, (cx, cy), 4, (0, 0, 255), -1)
+    for box, vehicle_id in zip(detected_boxes, vehicle_ids):
+        box_x1, box_y1, box_x2, box_y2 = int(box[0]), int(box[1]), int(box[2]), int(box[3])
+        center_x = int(box[0] + box[2]) // 2
+        center_y = int(box[1] + box[3]) // 2
+        cv2.circle(frame, (center_x, center_y), 4, (0, 0, 255), -1)
         cv2.putText(
             frame,
-            f"Id {id}",
-            (cx, cy),
+            f"Id {vehicle_id}",
+            (center_x, center_y),
             cv2.FONT_HERSHEY_SIMPLEX,
             1,
             (0, 0, 255),
             2,
         )
 
-
         #####going down#####
-        # if cy1 < (cy + offset) and cy1 > (cy - offset):
-        if 529 < cx < 680 and cy3 - offset < cy < cy3 + offset:
-            cv2.line(frame, (529, cy3), (680, cy3), (0, 255, 0), 3)
-
-            vh_down[id] = time.time()
-        if id in vh_down:
-
-            # if cy2 < (cy + offset) and cy2 > (cy - offset):
-            if 537 < cx < 736 and cy4 - offset < cy < cy4 + offset:
-                elapsed_time = time.time() - vh_down[id]
-                if counter.count(id) == 0:
-                    counter.append(id)
-                    vh_down.pop(id)
+        if 529 < center_x < 680 and line_y3 - line_offset < center_y < line_y3 + line_offset:
+            cv2.line(frame, (529, line_y3), (680, line_y3), (0, 255, 0), 3)
+            vehicles_down[vehicle_id] = time.time()
+        if vehicle_id in vehicles_down:
+            if 537 < center_x < 736 and line_y4 - line_offset < center_y < line_y4 + line_offset:
+                elapsed_time = time.time() - vehicles_down[vehicle_id]
+                if down_counter.count(vehicle_id) == 0:
+                    down_counter.append(vehicle_id)
+                    vehicles_down.pop(vehicle_id)
                     distance = 20  # meters
-                    a_speed_ms = distance / elapsed_time
-                    a_speed_kh = a_speed_ms * 3.6
-                    # print(a_speed_kh, 'km/h')
-                    cv2.circle(frame, (cx, cy), 4, (0, 0, 255), -1)
-                    cv2.putText(frame, str(id), (x3, y3), cv2.FONT_HERSHEY_COMPLEX, 0.6, (255, 255, 255), 1)
-                    cv2.putText(frame, str(int(a_speed_kh)) + 'Km/h', (x4, y4), cv2.FONT_HERSHEY_COMPLEX, 0.8,
+                    avg_speed_ms = distance / elapsed_time
+                    avg_speed_down = avg_speed_ms * 3.6
+                    cv2.circle(frame, (center_x, center_y), 4, (0, 0, 255), -1)
+                    cv2.putText(frame, str(vehicle_id), (box_x1, box_y1), cv2.FONT_HERSHEY_COMPLEX, 0.6, (255, 255, 255), 1)
+                    cv2.putText(frame, str(int(avg_speed_down)) + 'Km/h', (box_x2, box_y2), cv2.FONT_HERSHEY_COMPLEX, 0.8,
                                 (0, 255, 255), 2)
-                    cv2.line(frame, (537, cy4), (736, cy4), (0, 255, 0), 3)
-
+                    cv2.line(frame, (537, line_y4), (736, line_y4), (0, 255, 0), 3)
 
         #####going UP#####
-        if 75 < cx < 443 and cy2 - offset < cy < cy2 + offset:
-        # if cy2 < (cy + offset) and cy2 > (cy - offset):
-            cv2.line(frame, (75, cy2), (443, cy2), (0, 255, 0), 3)
-
-            vh_up[id] = time.time()
-        if id in vh_up:
-
-            if 162 < cx < 456 and cy1 - offset < cy < cy1 + offset:
-            # if cy1 < (cy + offset) and cy1 > (cy - offset):
-                elapsed1_time = time.time() - vh_up[id]
-
-                if counter1.count(id) == 0:
-                    counter1.append(id)
-                    vh_up.pop(id)
+        if 75 < center_x < 443 and line_y2 - line_offset < center_y < line_y2 + line_offset:
+            cv2.line(frame, (75, line_y2), (443, line_y2), (0, 255, 0), 3)
+            vehicles_up[vehicle_id] = time.time()
+        if vehicle_id in vehicles_up:
+            if 162 < center_x < 456 and line_y1 - line_offset < center_y < line_y1 + line_offset:
+                elapsed1_time = time.time() - vehicles_up[vehicle_id]
+                if up_counter.count(vehicle_id) == 0:
+                    up_counter.append(vehicle_id)
+                    vehicles_up.pop(vehicle_id)
                     distance1 = 20  # meters
-                    a_speed_ms1 = distance1 / elapsed1_time
-                    a_speed_kh1 = a_speed_ms1 * 3.6
-                    cv2.circle(frame, (cx, cy), 4, (0, 0, 255), -1)
-                    cv2.putText(frame, str(id), (x3, y3), cv2.FONT_HERSHEY_COMPLEX, 0.6, (255, 255, 255), 1)
-                    cv2.putText(frame, str(int(a_speed_kh1)) + 'Km/h', (x4, y4), cv2.FONT_HERSHEY_COMPLEX, 0.8,
+                    avg_speed_ms1 = distance1 / elapsed1_time
+                    avg_speed_up = avg_speed_ms1 * 3.6
+                    cv2.circle(frame, (center_x, center_y), 4, (0, 0, 255), -1)
+                    cv2.putText(frame, str(vehicle_id), (box_x1, box_y1), cv2.FONT_HERSHEY_COMPLEX, 0.6, (255, 255, 255), 1)
+                    cv2.putText(frame, str(int(avg_speed_up)) + 'Km/h', (box_x2, box_y2), cv2.FONT_HERSHEY_COMPLEX, 0.8,
                                 (0, 255, 255), 2)
-                    cv2.line(frame, (168, cy1), (456, cy1), (0, 255, 0), 3)
+                    cv2.line(frame, (168, line_y1), (456, line_y1), (0, 255, 0), 3)
 
-
-        # cv2.rectangle(frame, (x3, y3), (x4, y4), (0, 0, 255), 2)
-        cv2.putText(frame, str(id), (x3, y3), cv2.FONT_HERSHEY_COMPLEX, 0.6, (255, 0, 255), 2)
+        cv2.putText(frame, str(vehicle_id), (box_x1, box_y1), cv2.FONT_HERSHEY_COMPLEX, 0.6, (255, 0, 255), 2)
         # up line
-        cv2.line(frame, (168, cy1), (456, cy1), (0, 0, 255), 1)
-        cv2.line(frame, (75, cy2), (443, cy2), (0, 0, 255), 1)
+        cv2.line(frame, (168, line_y1), (456, line_y1), (0, 0, 255), 1)
+        cv2.line(frame, (75, line_y2), (443, line_y2), (0, 0, 255), 1)
         # down line
-        cv2.line(frame, (529, cy3), (680, cy3), (0, 0, 255), 1)
-        cv2.line(frame, (537, cy4), (730, cy4), (0, 0, 255), 1)
-        # cv2.line(frame, (179, cy1), (417, cy1), (0, 0, 255), 1)
-        # cv2.line(frame, (100, cy2), (408, cy2), (0, 0, 255), 1)
+        cv2.line(frame, (529, line_y3), (680, line_y3), (0, 0, 255), 1)
+        cv2.line(frame, (537, line_y4), (730, line_y4), (0, 0, 255), 1)
 
-
-    # # cv2.putText(frame, ('L1'), (277, 320), cv2.FONT_HERSHEY_COMPLEX, 0.8, (0, 255, 255), 2)
-    #
-    # cv2.line(frame, (100, cy2), (408, cy2), (0, 255, 255), 1)
-    #
-    # # cv2.putText(frame, ('L2'), (182, 367), cv2.FONT_HERSHEY_COMPLEX, 0.8, (0, 255, 255), 2)
-    d = (len(counter))
-    u = (len(counter1))
-    # e=(len(tcount))
-    # print("jfuyfluy",e)
-    cv2.putText(frame, ('goingdown:-') + str(d), (15, 30), cv2.FONT_HERSHEY_COMPLEX, 0.8, (255, 0, 0), 2)
-    if int(a_speed_kh) >0:
-        cv2.putText(frame, ('speed:-') + str(int(a_speed_kh)) +(' km/h'), (15, 55), cv2.FONT_HERSHEY_COMPLEX, 0.8, (255, 0, 0), 2)
-    # print(str(int(a_speed_kh)))
-    cv2.putText(frame, ('goingup:-') + str(u), (15, 100), cv2.FONT_HERSHEY_COMPLEX, 0.8, (255, 0, 0), 2)
-    if int(a_speed_kh1) >0:
-        cv2.putText(frame, ('speed:-') + str(int(a_speed_kh1)) +(' km/h'), (15, 130), cv2.FONT_HERSHEY_COMPLEX, 0.8, (255, 0, 0), 2)
+    down_count = len(down_counter)
+    up_count = len(up_counter)
+    cv2.putText(frame, ('goingdown:-') + str(down_count), (15, 30), cv2.FONT_HERSHEY_COMPLEX, 0.8, (255, 0, 0), 2)
+    if int(avg_speed_down) > 0:
+        cv2.putText(frame, ('speed:-') + str(int(avg_speed_down)) +(' km/h'), (15, 55), cv2.FONT_HERSHEY_COMPLEX, 0.8, (255, 0, 0), 2)
+    cv2.putText(frame, ('goingup:-') + str(up_count), (15, 100), cv2.FONT_HERSHEY_COMPLEX, 0.8, (255, 0, 0), 2)
+    if int(avg_speed_up) > 0:
+        cv2.putText(frame, ('speed:-') + str(int(avg_speed_up)) +(' km/h'), (15, 130), cv2.FONT_HERSHEY_COMPLEX, 0.8, (255, 0, 0), 2)
 
     cv2.imshow("RGB", frame)
-    if cv2.waitKey(0) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-cap.release()
+video_capture.release()
 cv2.destroyAllWindows()
-
