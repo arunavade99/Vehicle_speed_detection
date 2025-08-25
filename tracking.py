cfb27702babf7dfa3f6af6b8f9213dc1b6d75ec7
arunavade99 <arunavade99@gmail.com>
2025-08-25 20:55:52 +0530

    Refactor tracking.py to enable mouse position tracking and switch video source to webcam

--- tracking.py
+++ tracking.py
@@ -8,32 +8,24 @@ import time
 # from sort import *
 
 model = YOLO('yolov8n.pt')
-# def RGB(event, x, y, flags, param):
-#     if event == cv2.EVENT_MOUSEMOVE:
-#         colorsBGR = [x, y]
-#         # print(colorsBGR)
-#
-# cv2.namedWindow('RGB')
-# cv2.setMouseCallback('RGB', RGB)
-# cap = cv2.VideoCapture(0)
-cap = cv2.VideoCapture('rtsp://admin:xiphos@123@192.168.1.9:554/mpeg4/ch4/sub/av_stream')
-# fps = int(cap.get(cv2.CAP_PROP_FPS))
+'''
+# to shee the mouse position
+def RGB(event, x, y, flags, param):
+    if event == cv2.EVENT_MOUSEMOVE:
+        colorsBGR = [x, y]
+        # print(colorsBGR)'''
 
-# my_file = open("coco.txt", "r")
-# data = my_file.read()
-# class_list = data.split("\n")
+cap = cv2.VideoCapture(0)
+# cap = cv2.VideoCapture('rtsp://admin:xiphos@123@192.168.1.9:554/mpeg4/ch4/sub/av_stream')
+# fps = int(cap.get(cv2.CAP_PROP_FPS))
 
 count = 0
 d = 0
 while True:
     ret, frame = cap.read()
-    # gaussian_3 = cv2.GaussianBlur(frame, (5, 5), 2.0)
-    # frame = cv2.addWeighted(frame, 2.0, gaussian_3, -0.9, 20)
     if not ret:
         break
-    # count += 1
-    # if count % 10 != 0:
-    #     continue
+
     frame = cv2.resize(frame, (1020, 500))
 
     results = model.track(frame, persist=True,conf=0.5, classes=[0],tracker="bytetrack.yaml",verbose=False)
@@ -58,17 +50,8 @@ while True:
                 2,
             )
 
-    #     cv2.putText(frame, "Person", (x1, y1), cv2.FONT_HERSHEY_COMPLEX, 0.8, (0, 255, 255), 2)
-
     cv2.rectangle(frame, (10, 15), (260, 55), (42, 219, 151), cv2.FILLED)
     cv2.putText(frame, ('No of Persons:-') + str(d), (15, 40), cv2.FONT_HERSHEY_COMPLEX, 0.8, (255, 0, 0), 2)
-    # if count % (fps*10)== 0:
-    #     t = time.strftime("%Y-%m-%d %H-%M-%S")
-    #     # t=d
-    #     # if(t!=d):
-    #     file = 'C:/Users/Administrator/Desktop/YOLOV8/yolov8counting-trackingvehicles-main/New folder/' + t + '.jpg'
-    #     cv2.imwrite(file, frame)
-
 
     cv2.imshow("RGB", frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
